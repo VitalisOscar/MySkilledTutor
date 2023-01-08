96856b6de1ac03d7dbe73bea12cfd43c217abb60
VitalisOscar <vitalisoscar25@gmail.com>
2023-01-08 12:50:56 +0300

    Added footer

--- config/site.php
+++ config/site.php
@@ -3,4 +3,5 @@
     return [
         'admin_notification_email' => env('ADMIN_NOTIFICATION_EMAIL'),
         'contact_phone' => env('CONTACT_PHONE'),
+        'contact_email' => env('CONTACT_EMAIL'),
     ];
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -77,6 +77,127 @@
         @yield('content')
     </main>
 
+    @if(!preg_match('/client/', $current_route->getName() ?? ''))
+    <footer class="pt-5 text-white">
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-sm-5">
+                    <div class="mb-3">
+                        <img class="img-fluid d-block mb-3" style="max-width: 200px;" src="{{ asset('static/img/logo.png') }}" alt="{{ config('app.name') }}" >
+
+                        <p>
+                            {{ config('app.name') }} helps students submit quality assignments on time at very pocket friendly pricing.
+                            We link clients' assignments with our verified tutors enabling them
+                            to scoop the best grades in their assignments leading
+                            to a better overall grade.
+                        </p>
+                    </div>
+
+                </div>
+
+                <div class="col-sm-7">
+                    <div class="row">
+
+                        <div class="col-sm">
+                            <div>
+                                <h6 class="heading text-white">Clients</h6>
+
+                                <ul class="list-unstyled">
+                                    <li class="mb-2 px-2">
+                                        <a href="{{ route('register') }}">
+                                            <i class="fa fa-caret-right mr-1"></i>Registration
+                                        </a>
+                                    </li>
+
+                                    <li class="mb-2 px-2">
+                                        <a href="{{ route('login') }}">
+                                            <i class="fa fa-caret-right mr-1"></i>Sign In
+                                        </a>
+                                    </li>
+
+                                    <li class="mb-2 px-2">
+                                        <a href="{{ route('client.dashboard') }}">
+                                            <i class="fa fa-caret-right mr-1"></i>My Account
+                                        </a>
+                                    </li>
+
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="col-sm">
+                            <div>
+                                <h6 class="heading text-white">More Links</h6>
+
+                                <ul class="list-unstyled">
+                                    <li class="mb-2 px-2">
+                                        <a href="{{ route('client.orders.create') }}">
+                                            <i class="fa fa-caret-right mr-1"></i>New Order
+                                        </a>
+                                    </li>
+
+                                    <li class="mb-2 px-2">
+                                        <a href="{{ route('admin.orders.all', 'active') }}">
+                                            <i class="fa fa-caret-right mr-1"></i>My Orders
+                                        </a>
+                                    </li>
+
+                                    <li class="mb-2 px-2">
+                                        <a href="{{ route('client.notifications') }}">
+                                            <i class="fa fa-caret-right mr-1"></i>Notifications
+                                        </a>
+                                    </li>
+
+                                    <li class="mb-2 px-2"><a href= "mailto:{{ config('site.contact_email') }}?subject=Site+Help"><i class="fa fa-caret-right mr-1"></i>Feedback</a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="col-sm">
+                            <div>
+
+                                <h6 class="heading text-white">Get in touch</h6>
+                                <ul class="list-unstyled">
+                                    {{-- <li class="px-1 mb-2">
+                                        <a href="">
+                                            <i class="fa fa-fw fa-map-marker mr-2 text-primary"></i>
+                                            Some Place
+                                        </a>
+                                    </li> --}}
+                                    <li class="px-1 mb-2">
+                                        <a href="tel:+254790210091">
+                                            <i class="fa fa-fw fa-phone mr-2 text-success"></i>
+                                            {{ config('site.contact_phone') }}
+                                        </a>
+                                    </li>
+                                    <li class="px-1">
+                                        <a href="mailto:info@automanual.co.ke">
+                                            <i class="fa fa-fw fa-envelope mr-2 text-warning"></i>
+                                            {{ config('site.contact_email') }}
+                                        </a>
+                                    </li>
+                                </ul>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="bg-dark p-2 text-center">
+            <small>&copy;{{ now()->year }} All Rights Reserved | {{ config('app.name') }}</small>
+        </div>
+
+    </footer>
+    @endif
+
     <script src="https://kit.fontawesome.com/ce4529ea37.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
